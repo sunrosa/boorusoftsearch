c451be2d6ed8475458531cd80f9041f06b711d5d
Ibis Sunrosa <drsunrosa@gmail.com>
2022-10-03 06:59:23 -0500

    Search in json

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,23 @@ use std::collections::HashMap;
 
 use gelbooru_api as gb;
 
+#[derive(serde::Deserialize)]
+struct UserTags {
+    search: Vec<String>,
+    scores: HashMap<String, i64>
+}
+
 #[tokio::main]
 async fn main() {
-    let mut scores = HashMap::<String, i32>::new();
-    scores = serde_json::from_str(std::fs::read_to_string("score.json").unwrap().as_str()).unwrap();
+    let mut user_tags: UserTags = serde_json::from_str(std::fs::read_to_string("tags.json").unwrap().as_str()).unwrap();
 
-    let mut results = std::vec::Vec::<(i32, gb::api::Post)>::new();
+    let mut results = std::vec::Vec::<(i64, gb::api::Post)>::new();
 
     let client = gb::Client::public();
-    let post_query = gb::posts().random(true).send(&client).await.unwrap();
+    let post_query = gb::posts().random(true).tags(&user_tags.search).send(&client).await.unwrap();
 
     for post in post_query.posts {
-        results.push((evaluate(&post, &mut scores), post))
+        results.push((evaluate(&post, &mut user_tags.scores), post))
     }
 
     results.sort_by(|a, b| b.0.cmp(&a.0));
@@ -27,8 +32,8 @@ async fn main() {
     }
 }
 
-fn evaluate(post: &gb::api::Post, scores: &mut HashMap<String, i32>) -> i32 {
-    let mut evaluation: i32 = 0;
+fn evaluate(post: &gb::api::Post, scores: &mut HashMap<String, i64>) -> i64 {
+    let mut evaluation: i64 = 0;
     for tag in post.tags() {
         evaluation += *scores.entry(tag.to_string()).or_default(); // No clue in hell why it requires a mutable reference for something that I DO NOT mutate. But it does.
     }
